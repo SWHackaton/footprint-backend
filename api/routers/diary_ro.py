a449ed2f8cdc2a8908548e00600104e9362c19d1
jisoolee11 <jisu6414@likelion.org>
2022-06-23 03:50:18 +0900

    router logic

--- api/routers/diary_ro.py
+++ api/routers/diary_ro.py
@@ -6,4 +6,12 @@ router = APIRouter(
     responses = {404: {"description": "Not Found"}},
 )
 
-# 다이어리 CRUD
+@router.post("/post")
+def postDiary(visit_id: int, store_name: str):
+    # visit table content update
+    pass
+
+@router.get("/read")
+def readDiary(visit_id: int, user_id: str):
+    #visit table 내용 불러와 보여주기
+    pass
