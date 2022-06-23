e88690591366f678dc8d0d0501d4f875acca0b2f
Junsu Min <sunsuking@gmail.com>
2022-06-24 01:45:24 +0900

    Store Router 수정

--- api/routers/store_ro.py
+++ api/routers/store_ro.py
@@ -15,17 +15,16 @@ router = APIRouter(
 get_db = database.get_db
 
 categories = {
-    'meal': [],
-    'drink': [],
-    'shopping': [],
-    'culture': [],
+    'meal': ['인도음식', '중식', '베트남음식', '참치회', '제과,베이커리', '중화요리', '분식', '장어', '오리'],
+    'drink': ['카페', '커피전문점'],
+    'shopping': ['의류판매', '화장품', '가발', '건강식품판매', '상가,아케이드', '남성의류ㅡ양복'],
+    'culture': ['컴퓨터학원', '노래방', '미용실', '음악학원', '음악', '미술학원'],
     'travel': [],
     'hotel': [],
     'subway': [],
     'bus': [],
     'airplane': []
 }
-
 
 @router.get("/search")
 def searchStore(q: Union[str, None] = None, db: Session = Depends(get_db)):
